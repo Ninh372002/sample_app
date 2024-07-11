edfd2dcc870cc3694a156d5d8b0b17c62ecd4a41
Ninh372002 <tuvitam01@gmail.com>
2024-07-11 22:38:12 +0700

    Chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   root "static_pages#home"
 
@@ -10,4 +8,9 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users, only: :show
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
