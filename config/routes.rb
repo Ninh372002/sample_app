d7ceae878137e7d781fef9fd4ba77d50dec6c492
Ninh372002 <tuvitam01@gmail.com>
2024-07-11 22:45:11 +0700

    Chapter9

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
-Rails.application.routes.draw do
-  root "static_pages#home"
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  resources :products
+  get 'demo_partials/new'
+  get 'demo_partials/edit'
   get 'static_pages/home'
   get 'static_pages/help'
-  resources :static_pages, only: %i(home help)
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/signup", to: "users#new"
-  post "/signup", to: "users#create"
-  resources :users, only: :show
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  resources :static_pages, only: %i[home help]
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-
+  # Defines the root path route ("/")
+  # root "posts#index"
 end
