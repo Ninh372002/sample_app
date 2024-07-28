53deb1f5b4962eb802dba1a39356823d2ebc6d9b
Ninh372002 <tuvitam01@gmail.com>
2024-07-28 21:45:39 +0700

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
-  def show; end
+  before_action :logged_in_user, except: %i[new create]
+  before_action :find_user, except: %i[index new create]
+  before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -19,18 +21,62 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit; end
 
-  def set_user
-    @user = User.find_by(id: params[:id])
-    return if @user
-
-    flash[:warning] = 'User not found!'
-    redirect_to root_path
+  def update
+    if @user.update user_params
+      flash[:success] = 'User updated sucessfully'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  def show; end
+
+  def index
+    @pagy, @users = pagy User.newest, items: Settings.digit_10
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = 'User deleted'
+    else
+      flash[:danger] = 'Delete fail!'
+    end
+    redirect_to users_path
+  end
+
+  private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
                                  :password_confirmation
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    redirect_to root_path
+    flash[:warning] = 'Not found users!'
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:error] = 'You cannot edit this account.'
+    redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 20_240_707_170_014) do
     t.datetime 'updated_at', null: false
     t.string 'password_digest'
     t.string 'remember_digest'
+    t.boolean 'admin', default: false
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end
